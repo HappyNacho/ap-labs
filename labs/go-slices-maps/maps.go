89f2de77dbc1520d0796225eb491df43492c651a
HappyNacho <git@happynacho.com>
2020-09-22 15:36:37 -0500

    Go slices and maps
    Signed-off-by: HappyNacho <git@happynacho.com>

--- labs/go-slices-maps/maps.go
+++ labs/go-slices-maps/maps.go
@@ -1,4 +1,23 @@
 package main
 
+import (
+	"strings"
+
+	"golang.org/x/tour/wc"
+)
+
+func WordCount(s string) map[string]int {
+	//creates the map with strings as keys and 0 initial value
+	stringmapcounter := make(map[string]int)
+
+	//the loop basically creates an entry for every word and if it already exists, adds a counter
+	for _, wordKey := range strings.Fields(s) {
+		stringmapcounter[wordKey]++
+	}
+
+	return stringmapcounter
+}
+
 func main() {
+	wc.Test(WordCount)
 }
--- labs/go-slices-maps/slices.go
+++ labs/go-slices-maps/slices.go
@@ -3,7 +3,23 @@ package main
 import "golang.org/x/tour/pic"
 
 func Pic(dx, dy int) [][]uint8 {
-     return [][]uint8{}
+
+	//creates the slice of slices of length dy
+	picture := make([][]uint8, dy)
+
+	for y := range picture {
+
+		//fills each position of dy with a slice of dx
+		picture[y] = make([]uint8, dx)
+
+		for x := range picture[y] {
+
+			//gradient value assignment
+			picture[y][x] = uint8((x + y) / 2)
+		}
+	}
+
+	return picture
 }
 
 func main() {
